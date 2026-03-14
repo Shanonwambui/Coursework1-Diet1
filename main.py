73f1ee5f7c7346c88c6f2919f32afaf1b80d98b4
Shanon Wambui <shanonwambui24@gmail.com>
2026-03-14 14:43:25 +0000

    App engine app

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    name = "Shanon Mwangi"
+    student_id = "S2561435"
+    now = datetime.now()
+
+    return f"""
+    <h1>Cloud Computing and Web Services</h1>
+    <h1>Coursework 1 - Diet 1</h1>
+    <h2>Task1 </h2>
+    <h3>App Engine app</h3>
+    <p>Name: {name}</p>
+    <p>Student ID: {student_id}</p>
+    <p>Date and Time: {now}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
